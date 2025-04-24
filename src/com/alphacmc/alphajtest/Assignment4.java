f752811e64d15c9225345ef9f618489d04376f59
yohira <yohira@alphacmc.co.jp>
2025-04-24 17:39:34 +0900

    課題4

--- src/com/alphacmc/alphajtest/Assignment4.java
+++ src/com/alphacmc/alphajtest/Assignment4.java
@@ -174,7 +174,6 @@ public class Assignment4 {
                 }
             }
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
